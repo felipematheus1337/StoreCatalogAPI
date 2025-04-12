5397becaee03b72a8478e095f2f3c2f0857cf022
felipematheus1337 <lipehbr@gmail.com>
2025-04-12 11:49:13 -0300

    Criado classes de dominio.

---
+++ Models/Categoria.cs
@@ -0,0 +1,10 @@
+﻿namespace StoreCatalogAPI.Models;
+
+public class Categoria
+{
+    public int CategoriaId { get; set; }
+
+    public string? Nome { get; set; }
+
+    public string? ImagemURL { get; set; }
+}
---
+++ Models/Produto.cs
@@ -0,0 +1,8 @@
+﻿namespace StoreCatalogAPI.Models;
+
+public class Produto
+{
+    public int ProdutoId { get; set; }
+
+
+}
