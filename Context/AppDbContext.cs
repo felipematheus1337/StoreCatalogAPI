7817371166ac6cb2f72de418312eb9ddffeabaab
felipematheus1337 <lipehbr@gmail.com>
2025-04-12 13:40:30 -0300

    Adicionado DataAnnotations para as entidades.!

--- Context/AppDbContext.cs
+++ Context/AppDbContext.cs
@@ -1,8 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using StoreCatalogAPI.Models;
 
 namespace StoreCatalogAPI.Context;
 
 public class AppDbContext : DbContext
 {
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {}
 
+    public DbSet<Categoria>? Categorias { get; set; }
+    public DbSet<Produto>? Produtos { get; set; }
 }
--- Program.cs
+++ Program.cs
@@ -1,15 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using StoreCatalogAPI.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+string mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection)));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
