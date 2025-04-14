ebeb7730f96acd741ff28fcd13574b76fa0f9446
felipematheus1337 <lipehbr@gmail.com>
2025-04-14 18:23:50 -0300

    adicionado repositorio para categoria.!

---
+++ Repositories/ICategoriaRepository.cs
@@ -0,0 +1,16 @@
+﻿using StoreCatalogAPI.Models;
+
+namespace StoreCatalogAPI.Repositories;
+
+public interface ICategoriaRepository
+{
+    IEnumerable<Categoria> GetCategorias();
+    Categoria GetCaregoria(int id);
+
+    Categoria Create(Categoria categoria);
+    Categoria Update (Categoria categoria);
+
+    Categoria Delete(int id);
+
+
+}
